4f04137121c73767090ffb1ddeb9261bbf167d0d
rubquinta <rubemo05@gmail.com>
2024-08-10 09:15:10 -0400

    new-workshop

--- app-cdk/lib/app-cdk-stack.ts
+++ app-cdk/lib/app-cdk-stack.ts
@@ -1,8 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AppCdkStack extends cdk.Stack {
 
